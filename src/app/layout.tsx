1ab8df5f5e1f619af70583b279d8d46fe08b0859
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-14 14:37:58 +0530

    Added Toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Barlow } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Barlow, Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`${geistSans.variable} ${geistMono.variable} ${barlowFont.variable} antialiased`} >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-4">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
       <h1 className="text-blue-500">Welcome to the couse !!</h1>
       <h1 className="text-blue-500 font-barlow">Welcome to the couse !!</h1>
 
