9d33d7022e757f68959955f54831a27bc4f3bf29
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-14 12:52:42 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div>
       <h1 className="text-blue-500">Welcome to the couse !!</h1>
       <h1 className="text-blue-500 font-barlow">Welcome to the couse !!</h1>
     </div>
